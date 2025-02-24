128b03c4f7616ea020a34d233cbe797d9eae8258
durgesh656 <durgeshkushwaha656@gmail.com>
2025-02-24 12:23:50 +0530

    [Durgesh Kushwaha] Add: 'ability for the greeting app to give greeting message based on user attributes' feature added✨

--- greeting_app/src/main/java/com/example/greeting_app/controller/GreetingServiceController.java
+++ greeting_app/src/main/java/com/example/greeting_app/controller/GreetingServiceController.java
@@ -3,6 +3,7 @@ package com.example.greeting_app.controller;
 import com.example.greeting_app.service.GreetingService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +19,11 @@ public class GreetingServiceController {
     @GetMapping
     public String sayHello() {
         return greetingService.getGreetingMessage();
+    }
+
+    // GET request to greet with firstname and lastname
+    @GetMapping("/data")
+    public String greetWithFullName(@RequestParam(required = false) String firstName, @RequestParam(required = false) String lastName){
+        return greetingService.customGreetingMessage(firstName,lastName);
     }
 }
